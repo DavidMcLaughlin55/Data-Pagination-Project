c8bb76c1e4bd005a1df3b38444e3d4874cf663e5
DavidMcLaughlin55 <49570547+DavidMcLaughlin55@users.noreply.github.com>
2021-11-16 17:23:43 -0500

    JS Update-1
    Added functions for displaying students and selecting page.

--- js/script.js
+++ js/script.js
@@ -1,29 +1,76 @@
-/*
-Treehouse Techdegree: Data Pagination and Filtering
-*/
+/* Treehouse Techdegree: Data Pagination and Filtering */
+
+//GLOBAL VARIABLES
+const itemsPerPage = 9;
+const searchBar= `<label for="search" class="student-search">
+<span>Search by name</span>
+<input id="search" placeholder="Search by name...">
+<button type="button"><img src="img/icn-search.svg" alt="Search icon"></button>
+</label>`;
+
+/* SearchBar Feature */
+const header = document.querySelector('.header');
+header.insertAdjacentHTML('beforeend', searchBar)
 
 
-
-/*
-For assistance:
-   Check out the "Project Resources" section of the Instructions tab
-   Reach out in your Slack community if you have questions
-*/
-
-
-
-/*
-Create the `showPage` function
-This function will create and insert/append the elements needed to display a "page" of nine students
-*/
-
-
+//FUNCTIONS
 
 /*
-Create the `addPagination` function
-This function will create and insert/append the elements needed for the pagination buttons
+showPage Function
+-Creates and appends the elements needed to display a page of nine students
 */
+function showPage(list, page){
+   const startIndex = (page * itemsPerPage) - itemsPerPage;
+   const endIndex = page * itemsPerPage;
+   const studentList = document.querySelector('.student-list');
+   studentList.innerHTML = '';
+   for (let i = 0; i < list.length; i++) {
+      if (i >= startIndex && i < endIndex) {
+         console.log("Adding student to display");
+      const studentDisplay =  
+         `<li class="student-item cf">
+         <div class="student-details">
+           <img class="avatar" src= ${list[i].picture.medium} alt="Profile Picture">
+           <h3>${list[i].name.first} ${list[i].name.last}</h3>
+           <span class="email">${list[i].email}</span>
+         </div>
+         <div class="joined-details">
+           <span class="date">Joined ${list[i].registered.date}</span>
+         </div>
+         </li>`;
+         studentList.insertAdjacentHTML('beforeend', studentDisplay);
+      };
+   };
+};
+
+/*
+addPagination Function
+-Creates and appends the elements required for the pagination buttons
+*/
+function addPagination(list){
+   const numOfPages = Math.ceil(list.length / itemsPerPage);
+   const linkList = document.querySelector('.link-list');
+   linkList.innerHTML = '';
+   for (let i = 1; i <= numOfPages; i++) {
+      const button = 
+      `<li>
+      <button type="button">${i}</button>
+      </li>`;
+      linkList.insertAdjacentHTML('beforeend', button);
+      document.querySelector('button').className = 'active';
+      linkList.addEventListener('click',(e) => {
+         if (e.target.tagName === 'BUTTON') {
+            console.log(e.target.tagName);
+            let active = document.querySelector('.active');
+            active.className = '';
+            e.target.className = 'active';
+            showPage(list, e.target.textContent);
+         };
+      });
+   };
+};
 
 
-
-// Call functions
+// CALL FUNCTIONS
+showPage(data, 1);
+addPagination(data);
