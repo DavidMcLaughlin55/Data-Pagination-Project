5753d8070a6ed72d1800e297470857eea64e6f3f
DavidMcLaughlin55 <49570547+DavidMcLaughlin55@users.noreply.github.com>
2021-11-18 15:52:54 -0500

    JS Update-2

--- js/script.js
+++ js/script.js
@@ -2,16 +2,17 @@
 
 //GLOBAL VARIABLES
 const itemsPerPage = 9;
-const searchBar= `<label for="search" class="student-search">
+const searchBar = `<label for="search" class="student-search">
 <span>Search by name</span>
 <input id="search" placeholder="Search by name...">
-<button type="button"><img src="img/icn-search.svg" alt="Search icon"></button>
+<button id ="search-button" type="button"><img src="img/icn-search.svg" alt="Search icon"></button>
 </label>`;
 
 /* SearchBar Feature */
 const header = document.querySelector('.header');
-header.insertAdjacentHTML('beforeend', searchBar)
-
+header.insertAdjacentHTML('beforeend', searchBar);
+const searchInput = document.querySelector('#search');
+const searchSubmitBtn = document.querySelector('#search-button');
 
 //FUNCTIONS
 
@@ -19,16 +20,15 @@ header.insertAdjacentHTML('beforeend', searchBar)
 showPage Function
 -Creates and appends the elements needed to display a page of nine students
 */
-function showPage(list, page){
+function showPage(list, page) {
    const startIndex = (page * itemsPerPage) - itemsPerPage;
    const endIndex = page * itemsPerPage;
    const studentList = document.querySelector('.student-list');
    studentList.innerHTML = '';
    for (let i = 0; i < list.length; i++) {
       if (i >= startIndex && i < endIndex) {
-         console.log("Adding student to display");
-      const studentDisplay =  
-         `<li class="student-item cf">
+         const studentDisplay =
+            `<li class="student-item cf">
          <div class="student-details">
            <img class="avatar" src= ${list[i].picture.medium} alt="Profile Picture">
            <h3>${list[i].name.first} ${list[i].name.last}</h3>
@@ -47,18 +47,18 @@ function showPage(list, page){
 addPagination Function
 -Creates and appends the elements required for the pagination buttons
 */
-function addPagination(list){
+function addPagination(list) {
    const numOfPages = Math.ceil(list.length / itemsPerPage);
    const linkList = document.querySelector('.link-list');
    linkList.innerHTML = '';
    for (let i = 1; i <= numOfPages; i++) {
-      const button = 
-      `<li>
+      const button =
+         `<li>
       <button type="button">${i}</button>
       </li>`;
       linkList.insertAdjacentHTML('beforeend', button);
       document.querySelector('button').className = 'active';
-      linkList.addEventListener('click',(e) => {
+      linkList.addEventListener('click', (e) => {
          if (e.target.tagName === 'BUTTON') {
             console.log(e.target.tagName);
             let active = document.querySelector('.active');
@@ -70,6 +70,27 @@ function addPagination(list){
    };
 };
 
+/*
+searchStudents Function
+-Filters students to be displayed on page based off search bar input.
+*/
+
+function searchStudents(list) {
+   let studentMatches = [];
+   for (let i = 0; i < list.length; i++) {
+      if (list[i].name.first.toLowerCase().includes(searchInput.value.toLowerCase()) ||
+         list[i].name.last.toLowerCase().includes(searchInput.value.toLowerCase())) {
+         studentMatches.push(list[i]);
+         console.log(studentMatches);
+         return studentMatches;
+      };
+   };
+   console.log(studentMatches);
+   showPage(studentMatches, 1);
+   addPagination(studentMatches);
+};
+
+searchInput.addEventListener('keyup', () => { searchStudents(data) });
 
 // CALL FUNCTIONS
 showPage(data, 1);
